be1c30ae3cd219eb0a3ccb7260dd496367ae2873
AN Long <aisk@users.noreply.github.com>
2022-08-10 21:32:14 +0800

    🎨 format codes

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ extern crate libc;
 
 use std::ffi::CString;
 
-
 pub struct FileLock {
     filename: String,
     fd: libc::c_int,
@@ -11,13 +10,20 @@ pub struct FileLock {
 
 impl FileLock {
     pub fn new(filename: &str) -> FileLock {
-        return FileLock{filename: filename.to_string(), fd: 0}
+        return FileLock {
+            filename: filename.to_string(),
+            fd: 0,
+        };
     }
 
     pub fn lock(&mut self) -> Result<(), errno::Errno> {
         unsafe {
             #[allow(temporary_cstring_as_ptr)]
-            let fd = libc::open(CString::new(self.filename.as_str()).unwrap().as_ptr(), libc::O_RDWR | libc::O_CREAT, 0o644);
+            let fd = libc::open(
+                CString::new(self.filename.as_str()).unwrap().as_ptr(),
+                libc::O_RDWR | libc::O_CREAT,
+                0o644,
+            );
             if fd < 0 {
                 return Err(errno::errno());
             }
