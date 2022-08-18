b03afda3a6abcade5ff2abad7231b51d3b6b6c8d
AN Long <aisk@users.noreply.github.com>
2022-08-18 22:58:55 +0800

    ✨ add mod function new

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,16 @@ pub mod windows;
 #[cfg(windows)]
 pub use windows::FileLock;
 
+pub fn new(filename: &str) -> FileLock {
+    return FileLock::new(filename);
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
-        let mut lock = super::FileLock::new("test.lock");
+        let mut lock = super::new("test.lock");
         lock.lock().unwrap();
         lock.unlock().unwrap();
     }
